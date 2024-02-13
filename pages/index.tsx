3216836bfa54f76668a19604f144708538624e83
watase-eth <seankwatase@gmail.com>
2024-02-12 16:46:46 -1000

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,101 +1,101 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button, createMerkleTreeFromAllowList, getProofsForAllowListEntry, useAddress, useContract, useTokenBalance } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { NextPage } from "next";
+import { useState } from "react";
+import { utils } from "ethers";
 
 const Home: NextPage = () => {
+  const allowList = [
+    {
+      "address": "<ALLOWLIST_ADDRESS>",
+      "maxClaimable": "<CLAIMABLE_AMOUNT>"
+    },
+    {
+      "address": "<ALLOWLIST_ADDRESS>",
+      "maxClaimable": "<CLAIMABLE_AMOUNT>"
+    }
+  ];
+
+  const [merkleRoot, setMerkleRoot] = useState<string | null>(null);
+
+  const generateMerkleTree = async () => {
+    const merkleTree = await createMerkleTreeFromAllowList(allowList);
+    setMerkleRoot(merkleTree.getHexRoot());
+  };
+
+  const getUserProof = async (address: string) => {
+    const merkleTree = await createMerkleTreeFromAllowList(allowList);
+    const leaf = {
+      "address": address,
+      "maxClaimable": "<CLAIMABLE_AMOUNT>"
+    };
+    const proof = await getProofsForAllowListEntry(merkleTree, leaf);
+    const proofHash = "0x" + proof[0].data.toString("hex");
+    return proofHash;
+  };
+
+  const address = useAddress();
+  const { contract: tokenContract } = useContract("<CONTRACT_ADDRESS>");
+  const { data: tokenBalance } = useTokenBalance(tokenContract, address);
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
-            <ConnectWallet />
+        <ConnectWallet />
+        {address && (
+          <div>
+            <div style={{
+                backgroundColor: "#222",
+                padding: "2rem",
+                borderRadius: "1rem",
+                textAlign: "center",
+                minWidth: "500px",
+                marginBottom: "2rem",
+                marginTop: "2rem",
+              }}>
+                <h1>Create Merkle Tree</h1>
+                <button
+                  onClick={generateMerkleTree}
+                  style={{
+                    padding: "1rem",
+                    borderRadius: "8px",
+                    backgroundColor: "#FFF",
+                    color: "#333",
+                    border: "none",
+                    cursor: "pointer",
+                    fontSize: "1rem",
+                  }}
+                >Generate</button>
+                {merkleRoot && (
+                  <p>Merkle Root Hash: {merkleRoot}</p>
+                )}
+              </div>
+              <div style={{
+                backgroundColor: "#222",
+                padding: "2rem",
+                borderRadius: "1rem",
+                textAlign: "center",
+                minWidth: "500px",
+              }}>
+                <h1>ERC-20 Airdrop</h1>
+                <h3>Token balance: {tokenBalance?.displayValue}</h3>
+                <Web3Button
+                  contractAddress="<CONTRACT_ADDRESS>"
+                  action={async (contract) => contract.call(
+                    "claim",
+                    [
+                      address,
+                      utils.parseEther("<CLAIMABLE_AMOUNT>"),
+                      [await getUserProof(address)],
+                      utils.parseEther("<CLAIMABLE_AMOUNT>"),
+                    ]
+                  )}
+                  onError={() => alert("Not eligible for airdrop or already claimed!")}
+                  onSuccess={() => alert("Airdrop claimed!")}
+                >Claim Airdrop</Web3Button>
+              </div>
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+        )}
       </div>
     </main>
   );
